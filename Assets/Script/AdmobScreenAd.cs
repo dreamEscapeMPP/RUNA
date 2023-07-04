e84bd61cec400dd9682aebf9294ab2b2134df678
seonga <76620387+Seonga02024@users.noreply.github.com>
2023-07-04 21:29:30 +0900

    Update AdmobScreenAd.cs

--- Assets/Script/AdmobScreenAd.cs
+++ Assets/Script/AdmobScreenAd.cs
@@ -20,8 +20,8 @@ public class AdmobScreenAd : MonoBehaviour
         });
 
 #if UNITY_ANDROID
-        //adUnitId = "ca-app-pub-3940256099942544/1033173712";
-        adUnitId = "ca-app-pub-4727835752295775/5917816098";
+        //adUnitId = "key";
+        adUnitId = "key"; // 보완상 삭제
 #elif UNITY_IOS
             adUnitId = "unexpected_platform";
 #else
